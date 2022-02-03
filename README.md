b9fce17c61049eb3d506b1df2d9ebbf2bb8c0959
Sasha Bychkov <aleksandar.bichkov@gmail.com>
2022-02-03 15:07:50 +0500

    Added typescript + eslint

---
+++ README.md
@@ -0,0 +1,4 @@
+"export NODE_OPTIONS=--openssl-legacy-provider;" was added to fix an error:
+https://onlinessolution.blogspot.com/2021/11/error-error0308010cdigital-envelope.html
+
+Both "react" and "react-dom" were added to devDependencies manually to fix other errors occured in Storybook.
---
+++ components/Button/Button.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react'
+
+import './Button.css'
+
+interface IButtonProps {
+  readonly primary: boolean
+  readonly size: string
+  readonly backgroundColor: string
+  readonly label: string
+  onClick(): void
+}
+
+const Button: FC<IButtonProps> = props => {
+  const {
+    size,
+    label,
+    primary,
+    backgroundColor,
+  } = props
+
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary'
+
+  return (
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size || 'medium'}`, mode].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  )
+}
+
+export default Button
